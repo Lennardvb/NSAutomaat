1122443f5dcdc62e5485dc17d82c97f13157ce1b
Thegamebaker <rick.brouwers@live.nl>
2016-10-12 19:35:04 +0200

    Bezig met voorbereiding voor Dictionary.

---
+++ BerekenTarief.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab3
+{
+    class BerekenTarief
+    {
+
+        Dictionary<string, int> Tarief = new Dictionary<string, int>();
+       
+
+       
+        
+    }
+}
---
+++ OverzichtReis.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab3
+{
+    public static class OverzichtReis
+    {
+        /*
+        public static string BerekenReis(string Startplaats, string Eindbestemming)
+        {
+           
+           string totaleReis = Startplaats + "-" + Eindbestemming;
+
+            return totaleReis;
+        }
+        */
+
+        private class Reis
+        {
+            public string Startplaats;
+            public string Eindbestemming;
+
+            public Reis(string start, string eind)
+            {
+                Startplaats = start;
+                Eindbestemming = eind;
+            }
+        }
+
+        private class ReisEqualityComparer : IEqualityComparer<Reis>
+        {
+            #region IEqualityComparer<Reis> Members
+
+            public bool Equals(Reis x, Reis y)
+            {
+                return ((x.Startplaats == y.Startplaats) & (x.Eindbestemming == y.Eindbestemming));
+            }
+
+            public int GetHashCode(Reis obj)
+            {
+                string combined = obj.Startplaats + "|" + obj.Eindbestemming.ToString();
+                return (combined.GetHashCode());
+            }
+
+            #endregion
+        }
+
+    }
+}
